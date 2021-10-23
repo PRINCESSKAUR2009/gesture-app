66d20b634cdc4f54730e0f4d76cdbf98fefb4f81
PRINCESSKAUR2009 <78291134+PRINCESSKAUR2009@users.noreply.github.com>
2021-10-23 09:36:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+    video =createCapture(VIDEO);
+    video.size(550 , 500 );
+
+    canvas =createCanvas(550 , 500 );
+    canvas.position(560 , 150);
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+
+function modelLoaded() {
+    console.log('PoseNet is Iniatialized !');
+}
+
+
+function draw() {
+    background('#EEFC06')
+}
+
+function gotPoses(results) {
+if(results.length>0)
+{
+    console.log(results);
+}
+}
